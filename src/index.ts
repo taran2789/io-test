abd2a058c6516084c9540f9bce0b00e7a3ab825a
deepali-arvind <deepali.arvind@metamagic.in>
2018-12-10 13:18:05 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,4 @@ export * from './module/dashboard/chart.loader.service';
 export * from './module/map/map.loader.service';
 export * from './module/services/icon/icon.mapping.config';
 export * from './module/services/data/common.data.service';
+//all test
